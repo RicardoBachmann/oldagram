44169f6155f10d7a503ea85e43b3ec4f2dc9295d
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2023-07-24 16:20:16 +0200

    Display posts array data

--- script.js
+++ script.js
@@ -32,9 +32,36 @@ const posts = [
 const mainEl = document.getElementById("main-el");
 
 function renderPostSections() {
+  let sections = "";
   for (let i = 0; i < posts.length; i++) {
-    console.log(posts[i]);
+    sections += `
+    <section class="post-content>"
+      <div class="container">
+        <div class="user-info">
+          <img class="user-avatar" src="${posts[i].avatar}" alt="User avatar"
+          <div>
+              <h2 class="user-name">${posts[i].name}</h2>
+              <p class="location">${posts[i].location}</p>
+          </div>
+        </div>
+      </div> 
+      <div>
+          <img class="post-img" src="${posts[i].post}" alt="Painted portrait of the user">
+      </div>  
+    </section> 
+
+    <section class="post-actions">
+        <div class="icons">
+            <button class="btn"><img class="icon-img like-icon" src="images/icon-heart.png" alt="heart"></button>
+            <button class="btn"><img class="icon-img" src="images/icon-comment.png" alt="comment on post"></button> 
+            <button class="btn"><img class="icon-img" src="images/icon-dm.png" alt="dm the user"></button>      
+        </div>
+        <p>${posts[i].comment}</p>
+        <p>${posts[i].likes}</p>
+    </section> 
+    `;
   }
+  mainEl.innerHTML = sections;
 }
 
 renderPostSections();
